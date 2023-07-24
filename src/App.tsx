f3e795fdaf529fce45eca5e2186e3ff7155e4985
YoungSeungJang <1345620@naver.com>
2023-07-24 23:14:01 +0900

    :sparkles:Feat: ts

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let data = {
+  name: 'my cafe',
+  category: 'western',
+  address: {
+    city: 'seoul',
+    detail: 'somewhere',
+    zipcode: 12341234,
+  },
+  menu: [
+    { name: 'coffee', price: 3000 },
+    { name: 'latte', price: 3500 },
+  ],
+};
+
 const App: React.FC = () => {
   return (
     <div className="App">
